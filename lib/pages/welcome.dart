74ab2fd654e323749dbc8a841a1da502c6a360ff
Vinícius Storch <viniciusstorch62@gmail.com>
2025-09-02 16:34:54 -0300

    Adição de menu lateral em welcome.dart

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -37,25 +37,72 @@ class _WelcomeState extends State<Welcome> {
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(30),
                 borderSide: BorderSide.none,
-
-
               )
-
-
             )
           ),
         ),
         backgroundColor: Color(0xFFEBDD6C),
       ),
-
-
-
-
-
-
-
-
-
+      drawer: Drawer(
+        backgroundColor: Color(0xFFEBDD6C),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            SizedBox(
+              height: 100,
+              child: DrawerHeader(
+                decoration: BoxDecoration(color: Color(0xFFE8CA42),
+                ),
+                child: Center(
+                  child: Text(
+                    "Menu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                ),),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Tela Inicial"),
+            ),
+            ListTile(
+              leading: Icon(Icons.pets),
+              title: Text("Meus Pets"),
+            ),
+            ListTile(
+              leading: Icon(Icons.business),
+              title: Text("PetShops favoritos"),
+            ),
+            ListTile(
+              leading: Icon(Icons.calendar_month),
+              title: Text("Meus Agendamentos"),
+            ),
+            ListTile(
+              leading: Icon(Icons.local_offer),
+              title: Text("Promoções"),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Meu Perfil"),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Configurações"),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text("Sair"),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text("Sobre"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
