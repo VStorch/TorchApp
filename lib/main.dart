b80264bed9c5eb10ceec5094c5820469bb798cee
leonardo <leo.cortelim@gmail.com>
2025-10-13 09:50:12 -0300

    cor arrumada

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:torch_app/pages/my_appointments_page.dart';
 import 'package:torch_app/pages_pet_shop/home_page_pet_shop.dart';
 import 'package:torch_app/pages_pet_shop/reviews.dart';
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
       ],
 
       // Página inicial
-      home: HomePagePetShop(),
+      home: MyAppointmentsPage(),
     );
   }
 }
