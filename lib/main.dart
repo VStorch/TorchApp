feb5a28d9917bd594e8c7e4f60c2adfe45203a75
LeonardoCortelim <leo.cortelim@gmail.com>
2025-11-11 20:07:59 -0300

    Implementação dos slots

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:torch_app/pages/loading_page.dart';
-import 'package:torch_app/pages/promotions_page.dart';
-import 'package:torch_app/pages_pet_shop/promotions.dart';
-import 'package:torch_app/pages_pet_shop/service_times.dart';
+import 'package:torch_app/pages_pet_shop/home_page_pet_shop.dart';
+import 'package:torch_app/pages_pet_shop/slots_service.dart';
+
 
 void main() {
   runApp(const MyApp());
