e7868d34ed054f3e6b87063897f9c397aeb2f4c8
leonardo <leo.cortelim@gmail.com>
2025-11-12 10:57:59 -0300

    Ajuste importante

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:torch_app/pages/loading_page.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
